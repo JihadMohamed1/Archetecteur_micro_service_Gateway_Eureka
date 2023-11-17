e5a8c2d794470dbb433b0f145d1fdfc82cf67832
jihad <totojihad2020@gmail.com>
2023-11-17 09:52:47 +0100

    configur static bugs fixed

---
+++ getwayserv/src/main/java/com/example/getwayserv/GetwayservApplication.java
@@ -0,0 +1,25 @@
+package com.example.getwayserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GetwayservApplication {
+
+    @Bean
+    RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/clients/**").uri("http://localhost:8081"))
+                .route(r -> r.path("/produits/**").uri("http://localhost:8082"))
+                .build();
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetwayservApplication.class, args);
+    }
+
+}
--- getwayserv/src/test/java/com/example/getwayserv/GetwayservApplicationTests.java
+++ getwayserv/src/test/java/com/example/getwayserv/GetwayservApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.gatewayservice;
+package com.example.getwayserv;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayServiceApplicationTests {
+class GetwayservApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
